58614f4aed5b76b40ecbcc7249bd289a939b89c9
raphzam <60793109+raphzam@users.noreply.github.com>
2021-06-18 14:40:35 -0400

    implemented multiply and divide functions in Calculator class

--- src/lambda/calculator.js
+++ src/lambda/calculator.js
@@ -6,6 +6,14 @@ class Calculator {
   subtract(x, y) {
     return x - y;
   }
+
+  multiply(x, y) {
+    return x * y;
+  }
+
+  divide(x, y) {
+    return x / y;
+  }
 }
 
 module.exports = new Calculator();
